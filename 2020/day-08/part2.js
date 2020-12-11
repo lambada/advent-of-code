28d832e06649510f50347cf38baff1d4c2e3d72f
Megan Walker <megan@meganwalker.me.uk>
2020-12-11 17:10:45 +0000

    Day 8 part 2

---
+++ 2020/day-08/part2.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+
+const input = fs.readFileSync('input.txt', 'utf8')
+const program = input.split('\n').map(line => {
+  return {
+    operation: line.split(' ')[0],
+    arg: Number.parseInt(line.split(' ')[1], 10)
+  }
+})
+
+const run = program => {
+  const visitedInstructions = new Set()
+  let acc = 0
+  let pc = 0
+  let looped = false
+  let finished = false
+  while (true) {
+    if (pc < 0) {
+      break
+    }
+    if (visitedInstructions.has(pc)) {
+      looped = true
+      break
+    }
+    if (pc >= program.length) {
+      finished = true
+      break
+    }
+    visitedInstructions.add(pc)
+    const instruction = program[pc]
+    switch (instruction.operation) {
+      case 'nop':
+        pc = pc + 1
+        break
+      case 'acc':
+        acc = acc + instruction.arg
+        pc = pc + 1
+        break
+      case 'jmp':
+        pc = pc + instruction.arg
+    }
+  }
+
+  return {
+    looped,
+    finished,
+    acc
+  }
+}
+
+let solution
+for(let i = 0; i < program.length; i++) {
+  if (program[i].operation === 'jmp') {
+    const modifiedProgram = JSON.parse(JSON.stringify(program))
+    modifiedProgram[i].operation = 'nop'
+    const result = run(modifiedProgram)
+    if (result.finished) {
+      solution = result
+      break
+    }
+  } else if (program[i].operation === 'nop') {
+    const modifiedProgram = JSON.parse(JSON.stringify(program))
+    modifiedProgram[i].operation = 'jmp'
+    const result = run(modifiedProgram)
+    if (result.finished) {
+      solution = result
+      break
+    }
+  }
+}
+
+console.log(solution)
